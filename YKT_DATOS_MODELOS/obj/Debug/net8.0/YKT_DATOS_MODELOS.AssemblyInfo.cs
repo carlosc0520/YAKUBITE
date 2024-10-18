5943aefd9343561b68d2765b408271203bbcb73a
carlosc0520 <ccarbajalmt0520@gmail.com>
2024-10-18 17:45:31 -0500

    act

--- YKT_DATOS_MODELOS/obj/Debug/net8.0/YKT_DATOS_MODELOS.AssemblyInfo.cs
+++ YKT_DATOS_MODELOS/obj/Debug/net8.0/YKT_DATOS_MODELOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YKT_DATOS_MODELOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25b07a7726d947b9e50f9198115c5a7d9085104")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5aa4ea4576569499dae47af6c286fce70db8264")]
 [assembly: System.Reflection.AssemblyProductAttribute("YKT_DATOS_MODELOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YKT_DATOS_MODELOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
